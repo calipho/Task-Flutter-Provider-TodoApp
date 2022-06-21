26a91d33bd49f3797a5eb7e9e42161292e490cd6
calipho <106757959+calipho@users.noreply.github.com>
2022-06-21 17:49:57 +0300

    done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:todo_app/models/todo.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/providers/todo_provider.dart';
 import 'package:todo_app/widgets/todo_tile.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,23 +10,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Todo"),
-        backgroundColor: Colors.amber.shade300,
-        actions: [
-          IconButton(
-            onPressed: () => GoRouter.of(context).push('/add'),
-            icon: const Icon(
-              Icons.add_box_outlined,
-              size: 30,
+        appBar: AppBar(
+          title: const Text("Todo"),
+          backgroundColor: Colors.amber.shade300,
+          actions: [
+            IconButton(
+              onPressed: () => GoRouter.of(context).push('/add'),
+              icon: const Icon(
+                Icons.add_box_outlined,
+                size: 30,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-          child: TodoTile(
-        todo: Todo(id: 1, title: "First ToDo"),
-      )),
-    );
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: context.watch<TodoProvider>().todos.length,
+            itemBuilder: (context, index) => TodoTile(
+                  todo: context.watch<TodoProvider>().todos[index],
+                )));
   }
 }
